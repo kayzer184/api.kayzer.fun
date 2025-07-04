80db295a62bb28258392aaa23ce453d00114f976
kayzer184 <toer0257@gmail.com>
2025-07-05 00:15:19 +0300

    [App] Add base functionality  (#5)
    * feat: initialize prisma schema
    
    * feat: initialize prisma migrations
    
    * feat: add appling prisma migrations
    
    * feat: add database service, unit test and module
    
    * feat: add database service export
    
    * feat: add global decorator to module
    
    * feat: add database & user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,7 +13,9 @@ import { AppService } from './app.service';
         DATABASE_URL: Joi.string().required(),
         PORT: Joi.number().port().required()
       })
-    })
+    }),
+    DatabaseModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
